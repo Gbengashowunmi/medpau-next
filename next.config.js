45a6a0a4c2df7134e935d189b6157e96ad37e5fc
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2023-02-08 16:10:19 +0100

    package.json

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
+  swcMinify:false,
 }
-
+ 
 module.exports = nextConfig
